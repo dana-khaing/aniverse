81f73c4b927108dc6c51c6247fc274244b02bf20
Dana Khaing <dana1352000.uk@gmail.com>
2026-07-15 01:52:32 +0100

    feat: add installable offline PWA shell

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { LocalDataProvider } from "@/components/providers/local-data-provider";
+import { PwaProvider } from "@/components/providers/pwa-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -36,7 +37,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <LocalDataProvider>{children}</LocalDataProvider>
+        <LocalDataProvider>{children}<PwaProvider/></LocalDataProvider>
         <Analytics />
         <SpeedInsights />
       </body>
